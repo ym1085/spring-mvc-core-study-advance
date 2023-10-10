2bb6f7fcdbaca3359341502c63f0286989efd2cd
ymkim <ymkim1085@funin.camp>
2023-10-10 22:33:43 +0900

    feat: Logging 실습해보기
    1. SLF4J -> Logback 등 간단하게 실습

---
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,30 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @GetMapping(value = "/log-test")
+    public String logTest() {
+        String name = "spring";
+
+        System.out.println("name = " + name);
+
+        log.trace("trace my log =" + name); // 연산이 일어나는 경우 (cpu, memory)
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
+        log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
+
+        return "ok";
+    }
+
+}

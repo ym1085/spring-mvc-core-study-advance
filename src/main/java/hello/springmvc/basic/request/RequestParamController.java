3a7879c2c652a1c21b5b451fca9467dddf4073df
ymkim <ymkim1085@funin.camp>
2023-10-20 00:39:55 +0900

    feat: @ModelAttribute 사용 및 정리
    1. ModelAttribute를 사용하여 파라미터 바인딩 진행

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -81,6 +83,29 @@ public class RequestParamController {
     public String requestParamMap(@RequestParam Map<String, Object>paramMap) {
 
         log.info("username = {}, age = {}", paramMap.getOrDefault("username", ""), paramMap.getOrDefault("age", 0));
+        return "ok";
+    }
+
+    /**
+     * HTTP 파라미터 // Form(폼) Request Body -> multipart/form-data 형식 -> application/json 전송 불가
+     * @ModelAttribute
+     *      -> 클라이언트의 폼(form) HTTP 본문 | 파라미터 바인딩을 위해 사용
+     *      -> 기본적으로 생성자를 통해 필드 값 셋팅, 나머지는 필드의 setter를 통해 셋팅
+     *      -> 생성자 | setter 없으면 값 셋팅이 안됨
+     *      -> 바인딩 타입이 맞지 않으면 자동 검증 -> BindingException 던질 듯
+     *
+     * @RequestBody
+     *      -> 클라이언트의 application/json 요청을 바인딩이 아닌 변환하기 위해 사용
+     *      -> ObjectMapper를 통해 JSON -> Java 객체로 변환하는 과정을 거침
+     *      -> MappingJackson2HttpMessageConverter의 지원을 통해 변환 과정을 거침
+     *      -> 기본 생성자를 통해 값을 셋팅하기 때문에, 생성자, setter 없이도 값 셋팅 가능
+     *      -> 내부적으로 Reflaction을 사용
+     */
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData) {
+        log.info("username = {}, age = {}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData = {}", helloData);
         return "ok";
     }
 }
